b51755f8d162bfa7ceb77004d859b6f5a9b262ef
Camilo <camilogomezr25@gmail.com>
2023-10-25 11:44:42 -0500

    Crear el modelo de cursos

---
+++ src/models/curso.js
@@ -0,0 +1,27 @@
+const { time } = require("console");
+const mongoose = require("mongoose"); //se importa el componenete mongoose
+
+const cursoSchema = mongoose.Schema({
+
+    nombre: {
+        type: String,
+        require: true
+    },
+    descripcion: {
+        type: String,
+        require: true
+    },
+    instructor: {
+        type: String,
+        require: true
+    },
+    prerequisitos: {
+        type: String,
+        require: true
+    },
+    duracion: {
+        type: time,
+        require: true
+    }
+
+})
