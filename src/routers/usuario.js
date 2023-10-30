c264937ee08bc59b57ff4900d98642af1e462768
Camilo <camilogomezr25@gmail.com>
2023-10-29 19:10:32 -0500

    Añadir modificar informacion de usuario

--- src/routers/usuario.js
+++ src/routers/usuario.js
@@ -9,5 +9,13 @@ router.post("/usuarios", (req, res) => {
         .then((data) => res.json(data)).catch((error) => res.json({ message: error }));
 });
 
+//modificar usuario:
+router.put("/usuarios/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, apellido, correo, cargo, identificacion, numIdentificacion, fechaNacimiento, NombreUsuario, contraseña } = req.body; //nuevos datos
+    usuarioSchema.updateOne({ _id: id }, { $set: { nombre, apellido, correo, cargo, identificacion, numIdentificacion, fechaNacimiento, NombreUsuario, contraseña } })//actualizar
+        .then((data) => res.json(data)).catch((error) => res.json({ message: error }))
+});
+
 
 module.exports = router; // exporta router y sus HTTP
